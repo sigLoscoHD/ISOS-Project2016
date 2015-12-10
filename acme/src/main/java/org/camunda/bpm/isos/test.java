9a862639d32019e6252938b457a3b60820f51e55
sigLoscoHD <matteo.cappella@studenti.unicam.it>
2015-12-10 16:10:21 +0100

    primo commit

---
+++ acme/src/main/java/org/camunda/bpm/isos/test.java
@@ -0,0 +1,9 @@
+package org.camunda.bpm.isos;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("acme App")
+public class test extends ServletProcessApplication {
+  // empty implementation
+}
